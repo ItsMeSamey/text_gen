b33bf1fc8c6719c1400b6d1920b91cbc24a4d49d
ItsMeSamey <sameychain5041@gmail.com>
2024-07-12 22:52:49 +0530

    restructuring

---
+++ rng.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+fn getPrng() std.Random.DefaultPrng {
+  if (@inComptime()) @compileError("Rng cannot be initilaized at comptime");
+  return std.Random.DefaultPrng.init(init: {
+    const bigTimestamp = std.time.nanoTimestamp();
+    const allegedTimestamp: i64 = @truncate(bigTimestamp ^ (bigTimestamp >> 64));
+    var timestamp: u64 = @bitCast(allegedTimestamp);
+    var seed: u64 = undefined;
+
+    std.posix.getrandom(std.mem.asBytes(&seed)) catch |e| {
+      std.log.err("Recoverable Error: RNG initialization failed:\n{}", .{e});
+      timestamp ^= @bitCast(std.time.microTimestamp());
+    };
+    break :init timestamp ^ seed;
+  });
+}
+
+/// RNG object to be used
+var Prng: ?std.Random.DefaultPrng = null;
+
+pub noinline fn random() std.Random {
+  if (Prng) |_| {
+    return Prng.?.random();
+  } else {
+    Prng = getPrng();
+    return Prng.?.random();
+  }
+}
+
