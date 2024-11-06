8b8fcff5f2e0ab7395928f59c02a6051f9a2328d
ItsMeSamey <sameychain5041@gmail.com>
2024-11-06 06:25:20 +0530

    updated reader

--- genMarkov.zig
+++ genMarkov.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Stats = @import("common/markov/markovStats.zig");
+const meta = @import("common/markov/meta.zig");
 
 const CpuEndianness = Stats.EndianEnum.fromEndian(@import("builtin").cpu.arch.endian());
 
@@ -64,28 +65,96 @@ fn GetMarkovGenFromRuntimeStats(stats: Stats) type {
   }
 }
 
-fn GetMarkovGen(comptime Key: type, comptime Val: type, comptime Endianness: Stats.EndianEnum) type {
+fn GetMarkovGen(Key: type, Val: type, Endianness: Stats.EndianEnum, ConversionContext: type) type {
+  const fnType = @TypeOf(ConversionContext.convert);
+  comptime std.debug.assert(fnType == fn (self: *ConversionContext, index: u32) ?[]const u8);
+
+  const read = struct {
+    fn f(data: anytype, offset: usize) void {
+      var retval = data[offset];
+      comptime if (CpuEndianness != Endianness) std.mem.byteSwapAllFields(std.meta.Child(@TypeOf(data)), &retval);
+      return retval;
+    }
+  }.f;
+
   return struct {
+    /// table of keys
+    keys: []const TableKey,
+    /// table of values to the keys
+    vals: []const TableVal,
+
     /// an array of saperate chain offsets inside of the `jt` and is again the result of `Base.flush`
-    carray: []u32,
+    carray: []meta.TableChain,
     /// index of the chain that is currently selected
     cindex: u32 = 0,
 
-    /// a kind of jump table that is result of `Base.flush`
-    jt: []const u8,
-    /// length of the markov chain
-    len: u8,
+    // converts the input key to a `word`
+    convTable: if (Key == u8) void else []u32,
+    // The random number generator
+    random: std.Random,
+
+    /// Allocator for convTable and possibly carray
+    allocator: std.mem.Allocator,
+    /// If we have to free carray
+    freeCarray: bool,
 
     const Self = @This();
+    const TableKey = meta.TableKey(Key, Val);
+    const TableVal = meta.TableVal(Key, Val);
 
-    pub fn gen(self: *Self) Key {
-      return "";
+    /// init this struct
+    /// NOTE: we try to automatically free `carray` if it is not contained in data
+    fn init(data: []const u8, carray: []u8, freeCarray: bool, allocator: std.mem.Allocator) Self {
+      _ = data;
+      return .{
+        .keys = {},
+        .vals = {},
+        .carray = carray,
+        .convTable = {},
+        .random = {},
+        .allocator = allocator,
+        .freeCarray = freeCarray,
+      };
     }
 
+    /// Generate a word
+    pub fn gen(self: *Self) []const u8 {
+      return self.cc.convert(self.genKey()) orelse self.gen();
+    }
+
+    /// Generate a key, a key may or may not translate to a full word
+    fn genKey(self: *Self) Key {
+      const offset = self.carray[self.cindex];
+      const key0 = read(self.keys, offset);
+      const key1 = read(self.keys, offset+1);
+      const vals = self.vals[key0.value..key1.value];
+
+      const context = struct {
+        target: TableKey,
+
+        pub fn lt(c: @This(), v: TableKey) std.math.Order {
+          var vCopy = v;
+          comptime if (CpuEndianness != Endianness) std.mem.byteSwapAllFields(TableKey, &vCopy);
+          return c.target.value <= vCopy.value;
+        }
+      };
+
+      const valIndex = std.sort.partitionPoint(TableKey, vals, context{ .target = self.random.float(Val) }, context.lt);
+      const keyNext = read(vals, valIndex);
+
+      self.carray[self.cindex] = key0.next + keyNext.subnext;
+
+      return key0.key;
+    }
+
+    /// Refresh the cindex to random
     pub fn refresh(self: *Self) void {
-      @compileError("TODO: Implement");
-      // In case we have multiple disconnected chains in the generated model
-      // This function should reandomly select one with the probability proportional to the number of nodes in that chain
+      self.cindex = self.random.intRangeLessThan(u32, 0, self.carray.len);
+    }
+
+    pub fn free(self: *Self) void {
+      if (Key != u8) { self.allocator.free(self.convTable); }
+      if (self.freeCarray) { self.allocator.free(self.carray); }
     }
   };
 }
