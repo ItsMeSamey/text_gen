a31699fdefc0f2922fc60533c8d17383ef5be7c7
ItsMeSamey <sameychain5041@gmail.com>
2024-11-07 06:25:30 +0530

    moved tests

--- common/markov/meta.zig
+++ common/markov/meta.zig
@@ -4,6 +4,16 @@ pub fn SizedUint(maxCap: comptime_int) type {
   return std.meta.Int(.unsigned, std.math.log2(maxCap << 1));
 }
 
+test SizedUint {
+  try std.testing.expect(SizedUint(0) == u0);
+  try std.testing.expect(SizedUint(1) == u1);
+  try std.testing.expect(SizedUint(2) == u2);
+  try std.testing.expect(SizedUint(3) == u2);
+
+  try std.testing.expect(SizedUint(std.math.maxInt(u16)) == u16);
+  try std.testing.expect(SizedUint(std.math.maxInt(u16) + 1) == u17);
+}
+
 pub fn Uint(comptime len: comptime_int, sliceType: type) type {
   const childSize = @sizeOf(std.meta.Elem(sliceType)) * 8;
   return std.meta.Int(.unsigned, childSize * len);
@@ -15,6 +25,9 @@ pub fn asUint(comptime len: comptime_int, slice: anytype) Uint(len, @TypeOf(slic
   return @bitCast(nonSentinel[0..len].*);
 }
 
+test asUint {
+  try std.testing.expect(asUint(5, "hello") == @as(u40, @bitCast([5]u8{'h', 'e', 'l', 'l', 'o'})));
+}
 // Convert array to a uint
 pub fn arrAsUint(arr: anytype) Uint(arr.len, @TypeOf(arr)) {
   return asUint(arr.len, arr);
